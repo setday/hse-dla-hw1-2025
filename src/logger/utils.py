52406c1c895abe8ba92ba6c8fa495e9ba625cdad
Alexander Serkov <32331578+setday@users.noreply.github.com>
2025-10-14 16:32:00 +0300

    Update logger interface to match spectrogram shape

--- src/logger/utils.py
+++ src/logger/utils.py
@@ -53,7 +53,7 @@ def plot_spectrogram(spectrogram, name=None):
         image (Image): image of the spectrogram
     """
     plt.figure(figsize=(20, 5))
-    plt.pcolormesh(spectrogram)
+    plt.pcolormesh(spectrogram.mean(dim=0))
     plt.title(name)
     buf = io.BytesIO()
     plt.savefig(buf, format="png")
